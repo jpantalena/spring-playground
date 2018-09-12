ab7ba1f7c93705c72fc14baff3c04db1dce962d6
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2018-09-12 15:19:17 -0600

    Unit 8 - EXERCISE 05

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,41 +2,41 @@ package com.example.demo;
 
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	@Profile("default")
-	public CommandLineRunner seedData(EmployeeRepository employeeRepository) {
-		return (args) -> {
-			employeeRepository.deleteAll();
-			Employee employee = new Employee();
-			employee.setName("Employee");
-			employee.setSalary(24);
-			employee.setUsername("employee");
-			employee.setPassword("{noop}my-employee-password");
-			employee.setRole("EMPLOYEE");
-			employeeRepository.save(employee);
+    @Bean
+    @Profile("default")
+    public CommandLineRunner seedData(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
+        return (args) -> {
+            employeeRepository.deleteAll();
+            Employee employee = new Employee();
+            employee.setName("Employee");
+            employee.setSalary(24);
+            employee.setUsername("employee");
+            employee.setPassword(passwordEncoder.encode("my-employee-password"));
+            employee.setRole("EMPLOYEE");
+            employeeRepository.save(employee);
 
-			Employee boss = new Employee();
-			boss.setName("Bossy Boss");
-			boss.setSalary(24);
-			boss.setUsername("boss");
-			boss.setPassword("{noop}my-boss-password");
-			boss.setRole("MANAGER");
-			employeeRepository.save(boss);
-		};
-	}
+            Employee boss = new Employee();
+            boss.setName("Bossy Boss");
+            boss.setSalary(24);
+            boss.setUsername("boss");
+            boss.setPassword(passwordEncoder.encode("my-boss-password"));
+            boss.setRole("MANAGER");
+            employeeRepository.save(boss);
+        };
+    }
 
 }
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.demo.config;
 
 import com.example.demo.service.EmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     EmployeeDetailsService employeeDetailsService;
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,7 +31,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // this line is the one that changes
-        auth.userDetailsService(employeeDetailsService);
+        auth
+                .userDetailsService(employeeDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
